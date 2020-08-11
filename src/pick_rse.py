ade9f00735283098c0bb91ee63b0ec0a4fc13efd
Eric Vaandering <ericvaandering@gmail.com>
2020-08-11 12:06:01 -0500

    Test with CERN

--- src/pick_rse.py
+++ src/pick_rse.py
@@ -1,29 +1,30 @@
 #! /usr/bin/env python
 
 from __future__ import print_function
-from rucio.client.client import Client
+
 import pprint
 import random
+
+from rucio.client.client import Client
 
 rucio = Client()
 
 tape_rses = rucio.list_rses('rse_type=TAPE\cms_type=test')
 rses_with_weights = []
 
-def weighted_choice(choices):
 
+def weighted_choice(choices):
     # from https://stackoverflow.com/questions/3679694/a-weighted-version-of-random-choice
     # Python 3.6 includes something like this in the random library itself
 
-   total = sum(w for c, w in choices)
-   r = random.uniform(0, total)
-   upto = 0
-   for c, w in choices:
-      if upto + w >= r:
-         return c
-      upto += w
-   assert False, "Shouldn't get here"
-
+    total = sum(w for c, w in choices)
+    r = random.uniform(0, total)
+    upto = 0
+    for c, w in choices:
+        if upto + w >= r:
+            return c
+        upto += w
+    assert False, "Shouldn't get here"
 
 
 for rse in tape_rses:
@@ -33,10 +34,11 @@ for rse in tape_rses:
     # pprint.pprint(values)
     # pprint.pprint(attrs)
 
-    quota = attrs.get('ddm_quota', 10e12)
+    quota = attrs.get('ddm_quota', 1e12)
+    if 'CERN' in rse['rse']:
+        quota = 20e12
     requires_approval = attrs.get('requires_approval', False)
     rses_with_weights.append(((rse['rse'], requires_approval), quota))
-
 
 pprint.pprint(rses_with_weights)
 

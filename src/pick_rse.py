432cbf0fd8d5e38d9668403f27c690ee93424290
Eric Vaandering <ericvaandering@gmail.com>
2020-08-11 11:53:25 -0500

    Build list

--- src/pick_rse.py
+++ src/pick_rse.py
@@ -8,12 +8,18 @@ import pprint
 rucio = Client()
 
 tape_rses = rucio.list_rses('rse_type=TAPE')
+rses_with_weights = []
 
 for rse in tape_rses:
     pprint.pprint(rse)
-    values = rucio.get_rse(rse['rse'])
+    # values = rucio.get_rse(rse['rse'])
     attrs = rucio.list_rse_attributes(rse['rse'])
-    pprint.pprint(values)
+    # pprint.pprint(values)
     pprint.pprint(attrs)
 
-    
+    quota = attrs.get('ddm_quota', 10e12)
+    requires_approval = attrs.get('requires_approval', False)
+    rses_with_weights.append(((rse['rse'], attrs['requires_approval']), quota))
+
+
+pprint(rses_with_weights)
